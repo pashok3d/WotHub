81c188a7eba40f064ea2efcb342d355ef70b4ff5
Pavel Tarashkevich <35535358+pashok3d@users.noreply.github.com>
2020-03-12 18:08:24 +0100

    Deployment fix

--- web_visual.py
+++ web_visual.py
@@ -37,42 +37,35 @@ def density(data, bins = 50j):
     
     return [colors,[xmin, xmax, ymin, ymax]]
 
+@st.cache
 def load_data(map):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     replays_path = os.path.join(dir_path,'pro_data')
-
-    st.text('dir_path: ' + dir_path)
-
+    
     data_frames_paths = []
     for r, d, f in os.walk(replays_path):
-        st.text('r: ')
-        st.text(r)
-        st.text('d: ')
-        st.text(d)
-        st.text('f: ')
-        st.text(f)
-        for file in f:
-            if '.csv' in file:
-                data_frames_paths.append(os.path.join(r,file))
+            for file in f:
+                if '.csv' in file:
+                    data_frames_paths.append(os.path.join(r,file))
     
     df = pd.DataFrame([])
     
     for frame_path in data_frames_paths:
         df = df.append(pd.read_csv(frame_path), ignore_index = True)
 
-    #df = df.loc[(df['map_name'] == maps[map_to_filter]),:] # FLAG Create local division
+    df = df.loc[(df['map_name'] == maps[map_to_filter]),:] # FLAG Create local division
 
     return df
 
+@st.cache
 def load_img(map_name):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = os.path.dirname(os.path.realpath(__file__))    
     img_dir = os.path.join(dir_path,'maps/images')
     img_path = os.path.join(img_dir, map_name + '.png')
     img = plt.imread(img_path)
     return img
 
 st.sidebar.title("WotHub")
-
 # choose parameters
 map_to_filter = st.sidebar.selectbox('Map', map_names, index = 15)
 team_to_filter = st.sidebar.radio('Team', (1,2))
@@ -80,22 +73,27 @@ clock_to_filter = st.sidebar.slider('Clock', 0, 600, 300)
 st.sidebar.markdown('`Made by Pavel Tarashkevich`')
 
 
-df = load_data(maps[map_to_filter]) 
+##df = load_data(maps[map_to_filter]) 
 img = load_img(maps[map_to_filter]) 
 
+##data_choice = df.loc[(df['team'] == team_to_filter) & (df['clock'] == clock_to_filter),:]
 
-#data_choice = df.loc[(df['team'] == team_to_filter) & (df['clock'] == clock_to_filter),:]
+##if not data_choice.empty:
+##    color_map = density(data_choice, 50j)
+##
+##    plt.style.use('classic')
+##    fig,ax = plt.subplots(figsize=(12, 12))
+##        
+##    ax.imshow(img, extent=[-500,500,-500,500]) 
+##    ax.imshow(color_map[0], extent=color_map[1])
+##    ax.set_axis_off()
+##
+##    st.pyplot()
+##else:
+##    st.text('No data.')
 
-#if not data_choice.empty:
-#    color_map = density(data_choice, 50j)
-#
-#    plt.style.use('classic')
-#    fig,ax = plt.subplots(figsize=(12, 12))
-#        
-#    ax.imshow(img, extent=[-500,500,-500,500]) 
-#    ax.imshow(color_map[0], extent=color_map[1])
-#    ax.set_axis_off()
-#
-#    st.pyplot()
-#else:
-#    st.text('No data.')
+fig,ax = plt.subplots(figsize=(12, 12))
+        
+ax.imshow(img, extent=[-500,500,-500,500]) 
+
+st.pyplot()
