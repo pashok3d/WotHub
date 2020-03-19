991f9a979bbf3ff87633b950b32435941926109d
Pavel Tarashkevich <35535358+pashok3d@users.noreply.github.com>
2020-03-19 12:02:25 +0100

    Preprocessing improved
    Significant improvements and fixes were made. Reducing amount of output data and using pandas functionality insted of dictionaries. Speed up about x4 times.

--- parser.py
+++ parser.py
@@ -7,8 +7,11 @@ for r, d, f in os.walk(replays_path):
         for file in f:
             if '.wotreplay' in file:
                 replays.append(file)
+    
+replays_count = len(replays)
 
-for replay in replays:
+for count, replay in enumerate(replays):
     output_name = replay[:-len('.wotreplay')] + '.json'
     path = 'wotreplay-parser.exe --parse --root ./ --type json --input ./replays/' + replay + ' --output ./raw_data/' + output_name 
     os.system(path)
+    print(str(count) + '/' + str(replays_count) + ' files preprocessed.', end='\r')
--- preprocessor.py
+++ preprocessor.py
@@ -10,15 +10,6 @@ def find_packet_index(lst, key, value):
             indices.append(i)
     return indices
 
-def position_to_column(data, index):
-    positions = data['position']
-    position = float(positions[index])
-    return position
-
-def feature_to_column(data,vehicles_dict,feature):
-    player_id = str(int(data['player_id']))
-    return vehicles_dict[player_id][feature]
-
 def nation_to_column(data):
     vehicle = data['vehicle'].split(':')
     return vehicle[0]
@@ -38,70 +29,39 @@ def type_to_column(data):
     vehicle_id = vehicle[1]
     return vehicles_data[vehicle_id]['type']
 
-def preprocess(data):
-    #packets 
-    packets = data['packets']
+def preprocess(data): 
+    #packets cleaning
+    packets = data['packets']   
     packets_10_index = find_packet_index(packets, 'type', 10)
     packets_10 = [packets[i] for i in packets_10_index]
-    packets = pd.DataFrame(packets_10)
-    
+    packets = pd.DataFrame(packets_10) #keep only type 10 packets
     packets = packets.dropna()
     packets = packets.astype({'clock': 'int32'})
-    packets.drop_duplicates(subset = ['clock','player_id'], keep = 'last', inplace = True) 
-    
-    packets['x'] = packets.apply(lambda x: position_to_column(x,0), axis=1)
-    packets['y'] = packets.apply(lambda x: position_to_column(x,1), axis=1)
-    packets['z'] = packets.apply(lambda x: position_to_column(x,2), axis=1)
-    
+    packets.drop_duplicates(subset = ['clock','player_id'], keep = 'last', inplace = True) #keep only one position per second
+    packets[['x','y','z']] = pd.DataFrame(packets.position.tolist(), columns=['x','y','z'])
     packets = packets.drop(columns = ['type','position','team'])
     
-    #summary
+    #map
     packets['map_name'] = data['map']
+    
+    #summary
     packets['client_version'] = data['summary']['clientVersionFromExe'] 
     packets['battle_type'] = data['summary']['battleType']
+    
     #vehicles
     vehicles = data['summary']['vehicles']
-    
-    vehicles_dict = {}
-    
-    for vehicle in vehicles:
-        vehicle_dict = {	
-            "id" : vehicles[vehicle]['avatarSessionID'],
-    		"nickname" : vehicles[vehicle]['name'],
-    		"team" : vehicles[vehicle]['team'],
-    		"vehicle" : vehicles[vehicle]['vehicleType']
-    	}
-        vehicles_dict[vehicle_dict['id']] = vehicle_dict
+    vehicles = pd.DataFrame.from_dict(vehicles, orient = 'index')
+    vehicles = vehicles[['avatarSessionID','name','team','vehicleType']]
+    vehicles = vehicles.rename({'avatarSessionID':'player_id','name':'nickname','vehicleType':'vehicle'}, axis = 1)
+    vehicles = vehicles.astype({'player_id': 'int64'})
+    packets = packets.merge(vehicles)
     
     #score_card
     results = data['score_card'][0] 
-    
     packets['arena_id'] = results['arenaUniqueID'] 
     packets['winner_team'] = results['common']['winnerTeam'] 
     
-    vehicles_results = results['vehicles']
-    for player in vehicles_results:
-        vehicles_dict[player]['global_id'] = vehicles_results[player][0]['accountDBID']
-        vehicles_dict[player]['credits'] = vehicles_results[player][0]['credits']
-        vehicles_dict[player]['life_time'] = vehicles_results[player][0]['lifeTime']
-        vehicles_dict[player]['shots'] = vehicles_results[player][0]['shots']
-        vehicles_dict[player]['spotted'] = vehicles_results[player][0]['spotted']
-        vehicles_dict[player]['kills'] = vehicles_results[player][0]['kills']
-        vehicles_dict[player]['spotted'] = vehicles_results[player][0]['spotted']
-        vehicles_dict[player]['mileage'] = vehicles_results[player][0]['mileage']
-        vehicles_dict[player]['piercings'] = vehicles_results[player][0]['piercings']
-        vehicles_dict[player]['xp'] = vehicles_results[player][0]['xp']
-        vehicles_dict[player]['death_reason'] = vehicles_results[player][0]['deathReason']
-    
-    frags = data['score_card'][2]
-    for player in frags:
-        vehicles_dict[player]['frags'] = frags[player]['frags']
-        
-    #adding data to packets
-    features_to_add = ['global_id','nickname','team','vehicle']
-    for feature in features_to_add:
-      packets[feature] = packets.apply(lambda x: feature_to_column(x,vehicles_dict,feature), axis=1)  
-
+    #vehicles_data
     packets['nation'] = packets.apply(nation_to_column, axis=1)
     packets['vehicle_name'] = packets.apply(vehicle_name_to_column, axis=1)
     packets['tier'] = packets.apply(tier_to_column, axis=1)
@@ -109,7 +69,7 @@ def preprocess(data):
     packets = packets.drop('vehicle', axis = 1)
 
     return packets.reset_index(drop=True)
-    
+
 if __name__ == '__main__':
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -117,20 +77,24 @@ if __name__ == '__main__':
     pro_data_path = os.path.join(dir_path,'pro_data')
     
     files = []
+    print('Loading files...', end='\r')
     for r, d, f in os.walk(raw_data_path):
         for file in f:
             if '.json' in file:
                 files.append(file)
 
-    for file_name in files:
+    files_count = len(files)
+
+    for count, file_name in enumerate(files):
+        print(str(count) + '/' + str(files_count) + ' files preprocessed.', end='\r')
         file_path = os.path.join(raw_data_path,file_name)
-        print(file_path)
         file_desc = open(file_path, encoding="utf8")
         data = json.load(file_desc)
+
         try:
             processed_data = preprocess(data)
         except:
-            print('Failed to process file.')
+            print('Failed to process file.', end='\r')
             continue  
 
         file_name = processed_data['map_name'][0] + '.csv'
@@ -140,6 +104,3 @@ if __name__ == '__main__':
             processed_data = processed_data.append(pd.read_csv(file_path), ignore_index = True)
 
         processed_data.to_csv(file_path, index = False)
-
-
-
